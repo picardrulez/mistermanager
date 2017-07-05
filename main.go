ab90ebe52fd4768b2cd5143e894b218936c23086
Geoff <picardrulez@gmail.com>
2017-07-05 13:44:46 -0400

    showing local version on version page

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 //Set global vars
-var version string = "v1.0.2"
+var version string = "v1.0.3"
 var logfile string = "/var/log/mistermanager"
 var myuser string = "root"
 var myhome string = "/var/lib/mistermanager"
@@ -50,10 +50,27 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func versionHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Mister Manager "+version)
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Println("error getting hostname")
+	}
 	var config = ReadConfig()
 	notifyManagers := config.Managers
 	versionPath := config.VersionPath
+	stripQuotes := strings.Replace(versionPath, "\\", "", -1)
+	localURL := "http://" + hostname + stripQuotes
+	response, err := http.Get(localURL)
+	if err != nil {
+		log.Println("error pulling version from " + hostname)
+		log.Printf("%s", err)
+	}
+	pageContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("error reading version page contents from " + hostname)
+		log.Printf("%s", err)
+	}
+	stringPageReturn := string(pageContent)
+	io.WriteString(w, stringPageReturn+"\n")
 	var versionchan chan string = make(chan string)
 	if len(notifyManagers) != 0 {
 		for i := 0; i < len(notifyManagers); i++ {

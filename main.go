580e8cdb4e965822b2fca15274a1777fde355730
Geoff <picardrulez@gmail.com>
2017-07-05 16:19:48 -0400

    fixing array

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 //Set global vars
-var version string = "v1.0.7"
+var version string = "v1.0.8"
 var logfile string = "/var/log/mistermanager"
 var myuser string = "root"
 var myhome string = "/var/lib/mistermanager"
@@ -83,7 +83,7 @@ func versionHandler(w http.ResponseWriter, r *http.Request) {
 		var versionArray []string
 		for i := 0; i < len(notifyManagers); i++ {
 			versionReturn := <-versionchan
-			versionArray[i] = versionReturn
+			versionArray = append(versionArray, versionReturn)
 		}
 		sort.Strings(versionArray)
 		for i := 0; i < len(notifyManagers); i++ {
